28c1e5fc8dbf020fa215e80c1e9d203bc5017862
cjsks <cjsks765@gmail.com>
2023-10-23 23:43:11 +0900

    cj

--- app/src/main/java/com/example/myapplicationllll/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationllll/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.myapplicationllll
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {sdfsdfic
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
