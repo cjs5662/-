3cd827eb24010fca07436c44a8bfba7f34ed2276
Daeun <daeunchoi1004@naver.com>
2023-11-07 09:14:12 +0900

    dc

--- app/src/main/java/com/example/myapplicationllll/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationllll/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.myapplicationllll
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CalendarView
+import android.widget.TextView
+import java.sql.Date
+import java.text.DateFormat
+import java.text.SimpleDateFormat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val calendarView: CalendarView = findViewById(R.id.cal)
+        val textView: TextView = findViewById(R.id.t_text)
+
+        //날짜 형태
+        val dateFormat: DateFormat = SimpleDateFormat("yyyy년 MM월 dd일")
+
+        //today date
+        val date: Date = Date(calendarView.date)
+
+        //날짜변환
+        calendarView.setOnDateChangeListener( calendarView,
+
     }
 }
