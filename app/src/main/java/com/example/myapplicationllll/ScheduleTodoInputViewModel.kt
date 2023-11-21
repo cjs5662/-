8fae9fdf94ac5b22d1c737ec3160e8c913f4f447
cjsks <cjsks765@gmail.com>
2023-11-21 09:42:23 +0900

    cjs

--- app/src/main/java/com/example/myapplicationllll/ScheduleTodoInputViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myapplicationllll
-
-import androidx.lifecycle.ViewModel
-
-class ScheduleTodoInputViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/com/example/myapplicationllll/ScheduleTodoViewModel.kt
+++ app/src/main/java/com/example/myapplicationllll/ScheduleTodoViewModel.kt
@@ -1,9 +1,87 @@
 package com.example.myapplicationllll
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.database.*
 
 class ScheduleTodoViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val database = FirebaseDatabase.getInstance("https://planner-c04d6-default-rtdb.firebaseio.com/")
+    var scheduleList: MutableLiveData<List<Schedule>> = MutableLiveData()
+    var todoList: MutableLiveData<List<Todo>> = MutableLiveData()
+
+    // 일정, 할일 리스트 불러오기
+    fun fetchScheduleAndTodo() {
+        val scheduleRef = database.getReference("schedules")
+        scheduleRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val scheduleDataList = snapshot.children.mapNotNull { it.getValue(Schedule::class.java) }
+                scheduleList.value = scheduleDataList
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+        })
+
+        val todoRef = database.getReference("todos")
+        todoRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val todoDataList = snapshot.children.mapNotNull { it.getValue(Todo::class.java) }
+                todoList.value = todoDataList
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+        })
+    }
+
+    // 일정 저장
+    fun saveSchedule(schedule: Schedule) {
+        val scheduleRef = database.getReference("schedules")
+        scheduleRef.child(schedule.id).setValue(schedule)
+    }
+
+    // 할일 저장
+    fun saveTodo(todo: Todo) {
+        val todoRef = database.getReference("todos")
+        todoRef.child(todo.id).setValue(todo)
+    }
+
 }
 
-// 파이어베이스와 뷰모델 연동, 얼추 예쁘게 만들기(타협), 객체지향적 연결에 더 초점을 두기
+// 일정 클래스
+class Schedule {
+    lateinit var title: String
+    lateinit var time: String
+    lateinit var details: String
+    lateinit var reward: String
+    lateinit var id: String
+
+    constructor()
+
+    constructor(title: String, time: String, details: String, reward: String, id: String) {
+        this.title = title
+        this.time = time
+        this.details = details
+        this.reward = reward
+        this.id = id
+    }
+}
+
+// 할일 클래스
+class Todo {
+    lateinit var title: String
+    lateinit var time: String
+    lateinit var details: String
+    lateinit var reward: String
+    lateinit var id: String
+
+    constructor()
+
+    constructor(title: String, time: String, details: String, reward: String, id: String) {
+        this.title = title
+        this.time = time
+        this.details = details
+        this.reward = reward
+        this.id = id
+    }
+}
