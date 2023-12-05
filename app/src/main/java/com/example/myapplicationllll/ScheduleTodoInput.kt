22436ef2a584a660521b1d362262af56774bbd64
cjsks <cjsks765@gmail.com>
2023-12-05 18:39:53 +0900

    cjs

--- app/src/main/java/com/example/myapplicationllll/ScheduleTodoInput.kt
+++ app/src/main/java/com/example/myapplicationllll/ScheduleTodoInput.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
@@ -16,6 +18,7 @@ class ScheduleTodoInput : Fragment() {
     private lateinit var binding: FragmentScheduleTodoInputBinding
     private lateinit var viewModel: ScheduleTodoViewModel
     private val database = FirebaseDatabase.getInstance()
+    private var selectedDate: String = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,8 +33,44 @@ class ScheduleTodoInput : Fragment() {
 
         viewModel = ViewModelProvider(this).get(ScheduleTodoViewModel::class.java)
 
+        setupSpinners()
         setupButtons()
     }
+
+    // 년, 월, 일 스피너 설정
+    private fun setupSpinners() {
+        val years = Array(20) { i -> (i + 2023).toString() }
+        val months = Array(12) { i -> (i + 1).toString() }
+
+        val yearAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, years)
+        val monthAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, months)
+
+        binding.spinnerYear.adapter = yearAdapter
+        binding.spinnerMonth.adapter = monthAdapter
+
+        // 스피너 선택에 따른 출력
+        val onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val year = binding.spinnerYear.selectedItem.toString().toInt()
+                val month = binding.spinnerMonth.selectedItem.toString().toInt()
+                // 월에 따른 일수 보정
+                val days = when (month) {
+                    2 -> if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) 29 else 28
+                    4, 6, 9, 11 -> 30
+                    else -> 31
+                }
+                val dayAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, Array(days) { i -> (i + 1).toString() })
+                binding.spinnerDay.adapter = dayAdapter
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+        }
+
+        binding.spinnerYear.onItemSelectedListener = onItemSelectedListener
+        binding.spinnerMonth.onItemSelectedListener = onItemSelectedListener
+    }
+
 
     private fun setupButtons() {
         // 저장 버튼 클릭 리스너 설정
@@ -48,16 +87,19 @@ class ScheduleTodoInput : Fragment() {
     // 라디오 버튼에 따른 저장
     private fun saveInput() {
         val title = binding.etTitle.text.toString()
-        val time = binding.etTime.text.toString()
         val details = binding.etDetails.text.toString()
         val reward = binding.etReward.text.toString()
+        val year = binding.spinnerYear.selectedItem.toString()
+        val month = binding.spinnerMonth.selectedItem.toString()
+        val day = binding.spinnerDay.selectedItem.toString()
+        selectedDate = "$year-$month-$day"
 
         when (binding.radioGroup.checkedRadioButtonId) {
             R.id.radio_schedule -> {
-                saveSchedule(title, time, details, reward)
+                saveSchedule(title, selectedDate, details, reward)
             }
             R.id.radio_todo -> {
-                saveTodo(title, time, details, reward)
+                saveTodo(title, selectedDate, details, reward)
             }
             else -> {
                 // 라디오 버튼이 선택되지 않은 경우에 대한 처리
