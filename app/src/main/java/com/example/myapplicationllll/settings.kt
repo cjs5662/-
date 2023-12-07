be0d0800427ac5c8f891b4c3181c9ea2ce5c0b1b
jiyun2514 <yjiyun2514@gamil.com>
2023-12-07 14:11:59 +0900

    jy

--- app/src/main/java/com/example/myapplicationllll/settings.kt
+++ app/src/main/java/com/example/myapplicationllll/settings.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class planner_cal : Fragment() {
+class settings : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
